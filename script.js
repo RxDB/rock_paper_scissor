b4ffe12727f18f33ef95e04121e6d2c966ea44d1
RxDB <208150718+RxDB@users.noreply.github.com>
2025-12-16 17:14:28 +0530

    do 3 func

---
+++ script.js
@@ -0,0 +1,20 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function getComputerChoice(){
+    let a = Math.floor(Math.random()*3)
+    console.log(a);
+}
+
+function getHumanChoice(){
+    let b = parseInt(prompt('Enter Rock Paper Scissors'));
+}
+
+function playRound(humanChoice,computerChoice){
+    let humanChoice = humanChoice.toLowerCase();
+
+    
+} 
+
+getComputerChoice();
+getHumanChoice();
