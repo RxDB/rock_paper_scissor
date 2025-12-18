0657b785c18deeea58b624ad6e13fe3821bcaa11
RxDB <208150718+RxDB@users.noreply.github.com>
2025-12-18 14:47:43 +0530

    Finish Code

--- script.js
+++ script.js
@@ -3,18 +3,62 @@ let computerScore = 0;
 
 function getComputerChoice(){
     let a = Math.floor(Math.random()*3)
-    console.log(a);
+    return a;
 }
 
 function getHumanChoice(){
-    let b = parseInt(prompt('Enter Rock Paper Scissors'));
+    let b = prompt('Enter Rock Paper Scissors');
+    return b;
 }
 
-function playRound(humanChoice,computerChoice){
-    let humanChoice = humanChoice.toLowerCase();
+function playRound(hChoice,computerChoice){
+    let humanChoice = hChoice.toLowerCase();
 
+    if(computerChoice==0){
+        computerChoice = 'rock';
+    }else if(computerChoice==1){
+        computerChoice = 'paper';
+    }else if(computerChoice==2){
+        computerChoice = 'scissor';
+    }
+
+    if(humanChoice=='rock' && computerChoice=='paper'){
+        computerScore++;
+        console.log('Computer Wins');
+    }else if(humanChoice=='rock' && computerChoice=='scissor'){
+        humanScore++;
+        console.log('Human Wins');
+    }else if(humanChoice=='paper' && computerChoice=='rock'){
+        humanScore++;
+        console.log('Human Wins');
+
+    }else if(humanChoice=='paper' && computerChoice=='scissor'){
+        computerScore++;
+        console.log('Computer Wins');
+
+    }else if(humanChoice=='scissor' && computerChoice=='rock'){
+        computerScore++;
+        console.log('Computer Wins');
+    }else if(humanChoice=='scissor' && computerChoice=='paper'){
+        humanScore++;
+        console.log('Human Wins');
+
+    }
     
+
 } 
 
-getComputerChoice();
-getHumanChoice();
+function playGame(){
+    for(i=0;i<5;i++){
+        playRound(getHumanChoice(),getComputerChoice());
+    }
+}
+
+function showScore(){
+    console.log('Human Score',humanScore);
+    console.log('ComputerScore',computerScore);
+}
+
+playGame();
+showScore();
+
